8940641109a08548d93b7492709cfcaa00a010b8
jabrimohamed1-4060 <jabrimohamed1@gmail.com>
2025-09-22 22:02:36 +0100

    fix: Next.js output to .next (lowercase) + vercel config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-  // Ne PAS définir distDir en '.next' ; laisser par défaut = '.next'
+  reactStrictMode: true,
+  // Ne PAS définir distDir ici : par défaut c'est ".next" (minuscule)
+  // Si tu veux le forcer : distDir: '.next',
 };
+
 module.exports = nextConfig;
+
