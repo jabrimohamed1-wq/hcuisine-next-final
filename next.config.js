a7b404e2f9909ecbd02195bef8fe7800659d7264
jabrimohamed1-4060 <jabrimohamed1@gmail.com>
2025-09-22 23:39:43 +0100

    fix: Next.js config + Vercel preset + ignore build dirs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 ﻿/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Ne PAS définir distDir ici : par défaut c'est ".next" (minuscule)
-  // Si tu veux le forcer : distDir: '.next',
-};
-
+const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
 
